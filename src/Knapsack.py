ac98af2a38d805c910d8890e54a29f8a0aa73fc1
Tartyflaite <guillaumedlg@free.fr>
2022-01-01 20:57:52 +0100

    knwpsack optimal done

--- src/Knapsack.py
+++ src/Knapsack.py
@@ -23,6 +23,15 @@ class Knapsack:
         content += f"Le sac a {len(self.content)} objets, pour une valeur de {valeur} et un poids de {poids}/{self.capacity}"
         print(content)
 
+    def duplicate(self):
+        knapsack = Knapsack(self.capacity)
+        knapsack.content.extend(self.content)
+        return knapsack
+
+    def append_item(self, objects_dict, index):
+        if self.get_value_and_weight()
+
+
 
 def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
     items_sorted = sort_value_efficiency(objects_dict)
@@ -41,4 +50,22 @@ def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
 
 
 def solve_knapsack_optimal(knapsack, objects_dict) -> Knapsack:
-    return knapsack
+    return find_optimal(knapsack, objects_dict, 0)
+
+
+def find_optimal(knapsack: Knapsack, objects_dict: dict, index: int) -> Knapsack:
+    if (index >= len(objects_dict) - 1):
+        return knapsack
+
+    left = knapsack.duplicate()
+    right = knapsack.duplicate()
+
+    if right.append_item(objects_dict, index):
+        right = find_optimal(right, objects_dict, index + 1)
+
+    left = find_optimal(left, objects_dict, index + 1)
+
+    if left.get_value_and_weight(objects_dict)[0] > right.get_value_and_weight(objects_dict)[0]:
+        return left
+    return right
+
