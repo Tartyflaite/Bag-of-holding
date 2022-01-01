fa8bd818fd6ec2bcf020cd02b13ac2b6bf2f792c
Tartyflaite <guillaumedlg@free.fr>
2022-01-01 20:15:04 +0100

    knapsack greedy done

--- src/Knapsack.py
+++ src/Knapsack.py
@@ -17,15 +17,23 @@ class Knapsack:
         valeur = 0
         content = ""
         for obj in self.content:
-            content += obj + " " + objects_dict[obj] + "\n"
+            content += f"{obj} {objects_dict[obj][0]} {objects_dict[obj][1]}\n"
             poids += objects_dict[obj][1]
             valeur += objects_dict[obj][0]
-        content += "Le sac a" + len(
-            self.content) + "objets, pour une valeur de " + valeur + " et un poids de " + poids + "/" + self.capacity + "\n"
+        content += f"Le sac a {len(self.content)} objets, pour une valeur de {valeur} et un poids de {poids}/{self.capacity}"
+        print(content)
 
 
 def solve_knapsack_greedy(knapsack, objects_dict) -> Knapsack:
+    items_sorted = sort_value_efficiency(objects_dict)
+    for item in items_sorted:
+        if knapsack.get_value_and_weight(objects_dict)[1]+item[1][1] <= knapsack.capacity :
+            knapsack.content.append(item[0])
     return knapsack
+
+
+def sort_value_efficiency(objects_dic: dict):
+    return sorted(objects_dic.items(), key=lambda item: (item[1][0] / item[1][1]), reverse=True)
 
 
 def solve_knapsack_best(knapsack, objects_dict) -> Knapsack:
